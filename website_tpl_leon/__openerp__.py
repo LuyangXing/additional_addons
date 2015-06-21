6f219ea06d1f8ebc29256dd2a69a428af528778f
Luyang Xing <leon.luyangxing@gmail.com>
2015-06-21 21:12:49 +0800

    Initial commit

---
+++ website_tpl_leon/__openerp__.py
@@ -0,0 +1,24 @@
+{
+    # Theme information
+    'name': "website_tpl_leon",
+    'description': """
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates
+    'data': [
+        'views/options.xml',
+        'views/snippets.xml',
+    ],
+
+    # demo pages
+    'demo': [
+        'demo/pages.xml',
+    ],
+
+    # Your information
+    'author': "Your Company",
+    'website': "",
+}
